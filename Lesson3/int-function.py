a4879ffc964bfc6f20188e42a4741c928473f499
MaxxQwert <maxx.ivch@gmail.com>
2021-07-08 19:39:16 +0700

    Added decorator for degree_of_numbers

--- Lesson3/int-function.py
+++ Lesson3/int-function.py
@@ -6,6 +6,22 @@ keyword аргумент для выбора степени, в которую �
 - создать декоратор для замера времени выполнения функции
 """
 
+import time
+
+
+def timers(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        return_value = func(*args, **kwargs)
+        end = time.time()
+        if len(args) == 1:
+            typ = 'odd'
+        else:
+            typ = args[1]
+        print(f'Execution time of a function with an argument \'{typ}\' : {end - start} секунд.')
+        return return_value
+    return wrapper
+
 
 def is_prime(mum):
     if mum <= 2:
@@ -16,7 +32,7 @@ def is_prime(mum):
 def degree_of_numbers(numbers, degree=2):
     return list(map(lambda x: x ** degree, numbers))
 
-
+@timers
 def even_odd_prime(numbers, typ='even'):
     if typ == 'even':
         return list(filter(lambda x: x % 2 == 0, numbers))
@@ -28,10 +44,10 @@ def even_odd_prime(numbers, typ='even'):
     return 0
 
 
-a = list(i for i in range(20))
+a = list(i for i in range(10000))
 print(degree_of_numbers(a, 3))
 print(even_odd_prime(a))
 print(even_odd_prime(a, 'odd'))
 print(even_odd_prime(a, 'prime'))
 print(even_odd_prime(a, 'prme'))
-
+print(even_odd_prime(a, 'prime'))
