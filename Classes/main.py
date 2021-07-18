2ed6840462cc15a2089ec52059e47af95b95e3b1
MaxxQwert <maxx.ivch@gmail.com>
2021-07-18 15:19:00 +0700

    Start class Vehicle

--- Classes/main.py
+++ Classes/main.py
@@ -1,77 +1,91 @@
-class Vehicle(object):
-    #class variable shared between all instances of objects.
-    number_of_vehicles = 0
+from abc import ABCMeta, abstractmethod
 
-    def __init__(self,length,width,color,wheels):
-        self.length = length
-        self.width = width
-        self.color = color
-        self.wheels = wheels
-        Vehicle.number_of_vehicles += 1 #increasing class varaible count
 
-    def get_length(self):
-        print("I am %d meters long!"%self.length)
+class VehicleABC(metaclass=ABCMeta):
 
-    def get_wdith(self):
-        print("I am %d meters wide!"%self.width)
+    @abstractmethod
+    def Buzz(self):
+        raise NotImplementedError
 
-    def get_color(self):
-        print("My color is %s!"%self.color)
+    @abstractmethod
+    def start_engine(self, status: bool) -> bool:
+        raise NotImplementedError
 
-    def get_wheels(self):
-        print("I have %d number of wheels"%self.wheels)
+    @abstractmethod
+    def refuel(self, val: int) -> int:
+        raise NotImplementedError
 
-    #calling my methods so I don't need to call each of their own
-    def get_stats(self):
-        self.get_length()
-        self.get_wheels()
-        self.get_wdith()
-        self.get_color()
+    @abstractmethod
+    def draw(self, val: int) -> int:
+        raise NotImplementedError
 
-    def honk(self):
-        print("beep beep")
+
+class Vehicle(VehicleABC):
+
+    def __init__(self, type_vh, type_eng, volume_tank, fuel_cons, carrying, passenger):
+        self.type_vh = type_vh
+        self.type_eng = type_eng
+        self.volume_tank = volume_tank
+        self.fuel_cons = fuel_cons
+        self.carrying = carrying
+        self.passenger = passenger
+        self.volume_fuel = 0
+        self.status_engine = False
+
+    def Buzz(self):
+        print(f'{self.type_vh} gives a signal')
+
+    def start_engine(self, status: bool):
+        if self.volume_fuel == 0:
+            raise Exception('Not enough fuel')
+
 
 class Car(Vehicle):
     def vroom(self):
         print("Cars go vroom vroom")
 
+
 class Cooper(Car):
     def drift(self):
         print("Mini Coopers can drift well")
+
 
 class Airplanes(Vehicle):
     def fly(self):
         print("weeeeee I'm flying")
 
+
 class Tank(Vehicle):
-    #custom init because tanks have guns!~
-    #taking the gun size and tossing the rest of the arguments to the parent.
-    #if the parent doesn't find a __init__ it will keep going up until one is found or unelss we call it.
-    #Here we made a new __init__ so it doesn't go looking for one, but we call super() which is calling for the
-    #parent's __init__
-    def __init__(self,gun_size,*args):
+    # custom init because tanks have guns!~
+    # taking the gun size and tossing the rest of the arguments to the parent.
+    # if the parent doesn't find a __init__ it will keep going up until one is found or unelss we call it.
+    # Here we made a new __init__ so it doesn't go looking for one, but we call super() which is calling for the
+    # parent's __init__
+    def __init__(self, gun_size, *args):
         self.gun_size = gun_size
-        super(Tank,self).__init__(*args)
+        super(Tank, self).__init__(*args)
+
     def fire(self):
         print("pew pew pew")
 
-    #I have my custom get_stats but still calls parent's one so I don't repeat code.
+    # I have my custom get_stats but still calls parent's one so I don't repeat code.
     def get_stats(self):
-        print("my gun is this big: %d " %self.gun_size)
-        super(Tank,self).get_stats()
+        print("my gun is this big: %d " % self.gun_size)
+        super(Tank, self).get_stats()
 
-a = Cooper(150,150,"blue",4)
+
+a = Cooper(150, 150, "blue", 4)
 a.drift()
 a.vroom()
 a.honk()
 a.get_stats()
 print(a.number_of_vehicles)
 
-b = Airplanes(200,150,"white",2)
+b = Airplanes(200, 150, "white", 2)
 b.fly()
 print(b.number_of_vehicles)
 
-c = Tank(500,500,250,"Green",18)
+c = Tank(500, 500, 250, "Green", 18)
 c.fire()
 print(c.number_of_vehicles)
 c.get_stats()
